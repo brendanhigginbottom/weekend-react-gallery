6dff475d883aa9eb50dca80b7ecd51b41e123fde
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-10 17:37:54 -0500

    GET request working

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [galleryItems, setGalleryItems] = useState({});
+
+  const getGallery = () => {
+    //GET items from module
+    axios.get('/gallery').then((response) => {
+      setGalleryItems(response.data);
+      console.log(galleryItems[1]);
+    }).catch((error) => {
+      console.log(`Error in GET ${error}`);
+      alert('Something went wrong.');
+    });
+  }
+
+  useEffect(() => {
+    getGallery();
+  }, []);
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* <img src="images/goat_small.jpg"/> */}
+        <img src="images/bike.png" />
       </div>
     );
 }
