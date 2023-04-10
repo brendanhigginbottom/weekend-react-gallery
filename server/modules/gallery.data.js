197e17b676b145a7fb4064b27daa99061c0bc0cc
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-10 12:54:09 -0500

    added photos to galleryItems array

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/bike.png', description: 'Photo of my Surly Straggler. It\s a beauty.', likes: 0 },
+    { id: 3, path: 'images/potato.png', description: 'Photo of potatoes fondant.', likes: 0 },
+    { id: 4, path: 'images/Ride.png', description: 'Downtown as seen from a bike ride around Lake Harriet.', likes: 0 },
+    { id: 5, path: 'images/sweater.png', description: 'A sweet sweater my lovely partner made me.', likes: 0 },
+    { id: 6, path: 'images/tshirt.png', description: 'A shirt from a printmaker whose dumb shirts I always buy.', likes: 0 }
 ];
 
 module.exports = galleryItems;
